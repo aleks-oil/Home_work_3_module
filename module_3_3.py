819f26b2fe71fdac4e48fb13bc47c2612ba4770e
aleks-oil <aleksoil82@gmail.com>
2024-11-28 22:46:09 +0500

    28.11

---
+++ module_3_3.py
@@ -0,0 +1,16 @@
+print('практическое задание "Расваковка позиционных параметров"')
+def print_params (a = 1, b = 'строка', c = True):
+    print(a, b, c)
+
+values_list =(0.5, 'СТРОКА', False)
+value_dict = {'a': 10.5, 'b': 'Строка', 'c':30}
+value_list_2 = (54.32, 'Строка')
+
+print_params()
+print_params(1, 2, 3)
+print_params(c = [1, 2, 3])
+print_params(b = 25 )
+print_params(*values_list)
+print_params(**value_dict)
+print_params(*value_list_2, 42)
+print_params()
